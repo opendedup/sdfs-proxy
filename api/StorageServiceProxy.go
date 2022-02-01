13169eef65d57beee5b337076985736b2ea261a5
sam.silverberg@gmail.com <sam.silverberg@gmail.com>
2022-02-01 11:45:45 -0800

    added compression to proxy

--- api/StorageServiceProxy.go
+++ api/StorageServiceProxy.go
@@ -165,7 +165,6 @@ func (s *StorageServiceProxy) GetSparseDedupeFile(req *spb.SparseDedupeFileReque
 			fl, err := fi.Recv()
 			if err != nil {
 				return err
-
 			}
 			if err := stream.Send(fl); err != nil {
 				return err
